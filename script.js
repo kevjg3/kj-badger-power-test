4ee425dcb003d019176936047edaaf11941cadeb
kevjg3 <139889692+kevjg3@users.noreply.github.com>
2024-05-01 14:27:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ function power(time) {
     let lbs = document.getElementById("lbsInput").value;
     let kgs = lbs * 0.453592;
     
-    //document.getElementById("power").innerHTML = timelist[0];
-    //document.getElementById("velo").innerHTML = kgs;
-
-    
 
     let distance = [4.572, 9.144, 13.716, 18.288, 22.86]
     let count = 0;
     let strvelo = "Velocities in 5yd intervals: ";
     let strpower = "Power Output in 5yd increments: ";
+    let straccel = "Accel in 5yd increments: ";
     
     while (count < 5) {
         let velo = distance[count] / timelist[count];
-        //document.getElementById("velo").innerHTML = velo;
     let accel = velo / timelist[count];
     let force = kgs * accel;
     let work = force * distance[count];
@@ -26,15 +22,16 @@ function power(time) {
     if (count == 4){
         strpower += power + " Watts"
         strvelo += Math.round(velo*100)/100 + " m/s";
+        straccel += Math.round(accel*100)/100 + " m/s^2";
     }    
     else{
         strpower += power + " Watts, ";
         strvelo += Math.round(velo*100)/100 + " m/s, ";
+        straccel += Math.round(accel*100)/100 + " m/s^2, ";
     }
         document.getElementById("power").innerHTML = strpower;
       count++;
     }
-    //document.getElementById("power").innerHTML = Math.round(kgs*100)/100 + " kg";
-    //document.getElementById("power").innerHTML = strpower;
     document.getElementById("velo").innerHTML = strvelo;
+    document.getElementById("accel").innerHTML = straccel;
   }
